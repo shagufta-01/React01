e1387d34b25e29a0ba4ba7ed1f62bac95faa8434
Shagufta fatima <129382422+shagufta-01@users.noreply.github.com>
2024-07-27 16:34:36 +0530

    frist

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import TextForm from './Components/TextForm'
 import About from './Components/About'
 import { useState } from 'react'
 import Alert from './Components/Alert'
+
+
+
 function App() {
 const [mode, setMode] = useState('grey')
 const [alert, setAlert] = useState(null)
@@ -23,20 +26,29 @@ const toggleMode =()=>{
   setMode('dark')
   document.body.style.backgroundColor='grey'
 showAlert("Dark mode has been enable", "success")
+document.title ="TextUtilss - Dark Mode"
+// setInterval(()=>{
+//   document.title='TextUtils is Amazing Mode'
+// },2000)
+// setInterval(()=>{
+//   document.title ='Install TextUtils Now'
+// },1500) ///thisss is not a goood UI
  }
  else{
   setMode('light')
   document.body.style.backgroundColor='white'
   showAlert("Light mode has been enable", "success")
+  document.title ="TextUtilss - Light Mode"
  }
 }
   return (
   <>
-
 <Navbar title="TextUtils" aboutText="About us" mode={mode} toggleMode ={toggleMode}/>
 <Alert alert={alert}/>
 <div className='container my-3'>
-<TextForm heading="Enter the text to Analyzed " showaLert ={showAlert} mode={mode}/>
+ <TextForm heading="Enter the text to Analyzed " showaLert ={showAlert} mode={mode}/>
+{/* <TextForm heading="Enter the text to Analyzed " showaLert ={showAlert} mode={mode}/> */}
+ <About />
 {/* <About /> */}
 </div>
 {/* <Navbar /> */}
